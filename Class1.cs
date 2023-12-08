f9918c904be02f5a6dc06821beb8e073ed506c1f
avexyy <113837032+avexyy@users.noreply.github.com>
2023-12-08 17:04:15 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,47 @@
+﻿namespace Stirnreihe.Data;
+
+// person with FirstName, LastName, and Height properties
+public class Person
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public int Height { get; set; }
+
+    // overrides the ToString() method
+    public override string ToString()
+    {
+        return $"{FirstName}, {LastName} ({Height}cm)";
+    }
+}
+
+// node that contains a Person object and a reference to the next Node
+public class Node
+{
+    public Person? Person { get; set; }
+    public Node? Next { get; set; }
+}
+
+public class LineOfPeople
+{
+    public Node? First { get; set; } = null;
+
+    // adds a person to the front of the line
+    public void AddToFront(Person person)
+    {
+        var temp = First;  // stores the reference to the current first node
+        First = new Node();  // creates a new node for the new person
+        First.Person = person;  // sets the Person property of the new node
+        First.Next = temp;  // sets the next node to the previous first node
+    }
+
+    // clears the entire line of people
+    public void Clear()
+    {
+        while (First != null)
+        {
+            var temp = First;  // stores the reference to the current first node
+            First = First.Next;  // moves the first reference to the next node
+            temp.Next = null;  // unlinks the current node from the rest of the list
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using Stirnreihe.Data;
+System.Console.WriteLine("Welcome to the Stirnreihe World Record App! What do you want to do?");
+System.Console.WriteLine("1) Add a person to the line");
+System.Console.WriteLine("2) Print the line");
+System.Console.WriteLine("3) Clear the line");
+System.Console.WriteLine("4) Exit");
+System.Console.Write("Your choice: ");
+var input = Console.ReadLine()!;
+
+LineOfPeople lineOfPeople = new LineOfPeople(); // creating an instance of the LineOfPeople class to manage people in a line
+
+while (input != "4")
+{
+    switch (input)
+    {
+        case "1":
+            var p = new Person(); // creating a new Person object
+
+            Console.Write($"First Name: ");
+            string firstName = Console.ReadLine()!;
+
+            Console.Write($"Last Name: "); 
+            string lastName = Console.ReadLine()!;
+
+            Console.Write($"Height in cm: ");
+            int height = int.Parse(Console.ReadLine()!);
+            
+            p.FirstName = firstName; // setting the properties of the Person object
+            p.LastName = lastName;
+            p.Height = height;
+
+            lineOfPeople.AddToFront(p); // adding the created person to the front of the line
+            break;
+
+        case "2": // if user chooses option 2 - print the line
+            var current = lineOfPeople.First; // getting the reference to the first node in the line
+
+            while (current != null)
+            {
+                Console.WriteLine(current.Person);
+                current = current.Next; // moving to the next node in the line
+            }
+            break;
+
+        case "3": // if user chooses option 3 - clear the line
+            lineOfPeople.Clear(); // clears all people from the line
+            break;
+    }
+
+    Console.Write($"Your Choice: ");
+    input = Console.ReadLine();
+}
